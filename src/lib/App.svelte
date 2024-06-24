74d47dbeeecd7fd2e5ffd2409dbf2906f2cd0da0
Dan Edwards <daniel.edwards96@yahoo.com>
2024-06-24 20:31:32 +0100

    Install Tailwind

---
+++ src/lib/App.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+	import FilmList from './components/FilmList.svelte';
+	import type { Film } from '../types';
+
+	const VITE_serverURI = `http://localhost:5001/films/`;
+
+	let films: Film[] = [];
+	let filmsSeen: number = 0;
+
+	const fetchFilms = async () => {
+		try {
+			const response = await axios.get(VITE_serverURI);
+			films = response.data;
+			calculateFilmsSeen();
+		} catch (error) {
+			console.error('Error fetching films:', error);
+		}
+	};
+
+	const calculateFilmsSeen = () => {
+		filmsSeen = films.filter((film) => film.seen).length;
+	};
+
+	onMount(() => {
+		fetchFilms();
+	});
+
+	const toggleSeen = async (id: string) => {
+		try {
+			const response = await axios.post(
+				`http://localhost:5001/films/${id}/toggle`
+			);
+			films = films.map((film) => (film._id === id ? response.data : film));
+			calculateFilmsSeen();
+		} catch (error) {
+			console.error('Error toggling seen status:', error);
+		}
+	};
+</script>
+
+<main class="bg-gradient-to-bl from-blue-100 to-pink-100">
+	<h1 class="font-bold text-4xl">Metacritic's Top 100 Films</h1>
+	<p class="py-4">
+		A full-stack web app that only Dan can interact with. <span
+			class="font-bold">Films seen: {filmsSeen} / 100</span
+		>
+	</p>
+	<FilmList {films} {toggleSeen} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export interface Film {
+    _id: string;
+    title: string;
+    year: number;
+    rank: number;
+    metascore: number;
+    description: string;
+    seen: boolean;
+}
